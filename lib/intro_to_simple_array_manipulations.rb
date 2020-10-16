f9dbc5837de4851ddbe03e2bf5fd9cdb31bdfd0b
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-16 18:38:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_concat(all_my_favs, more_favs)
-  # all_my_favs = ["raindrops on roses", "whiskers on kittens"]
+  all_my_favs = ["raindrops on roses", "whiskers on kittens"]
   all_my_favs.concat(["sports cars", "flatiron school"])
   all_my_favs
 end
