b57b7b2bb63fc38b9b6e7cabe68e69584fc591fe
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-16 18:12:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,7 +21,7 @@ end
 
 def using_delete(array, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = instructors.delete("Steven")
+  instructors.delete_if {|instructor| instructor == "Steven"}
 end
   instructors
 end
