9cd758f2ede2ab332a58d7a81678b6b83545dcab
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-16 17:36:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,3 +6,5 @@ end
 
 def using_insert(array, element)
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  list_of_programming_languages.insert(4, "Python")
+end
