da3aba93e978da7757dd7a7d2ebd34085bfba0c0
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-16 17:32:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,6 @@ def using_concat(all_my_favs, more_favs)
   all_my_favs.concat(["sports cars", "flatiron school"])
   all_my_favs
 end
+
+def using_insert(array, element)
+  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
